67ec12cd0bc6dada01e75fcd81f75c4daee490f6
Thanush07RAW <draken0067@gmail.com>
2025-01-25 17:14:46 +0530

    Add files via upload

---
+++ Arthamatic.java
@@ -0,0 +1,10 @@
+public class Arthamatic{
+    public static void main(String[]args){
+        int a = 22;
+        int b = 44;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+    }
+}
